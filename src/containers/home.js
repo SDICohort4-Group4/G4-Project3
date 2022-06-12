f4e2c03f11092cce75d1cdde068d15b4b8da963d
Wee Tien Guan <98148142+tgweeSDICohort4@users.noreply.github.com>
2022-06-12 16:02:52 +0800

    item display setup

--- src/containers/home.js
+++ src/containers/home.js
@@ -3,15 +3,17 @@ import {useEffect, useState} from "react";
 import GetData from "../API/getData";
 
 
+
+
 export default function AccountScreen() {
 
-    const [itemData,setItemData]=useState([]);
+    const [itemData,setItemData]=useState();
 
     useEffect(()=>{
       
-        const dataType="/item/sku/filletofish"
+        const dataType="/item/brand/mac"
         GetData({dataType,getItemData});
-    
+           
     },[]);
 
     function getItemData(data){
@@ -19,26 +21,42 @@ export default function AccountScreen() {
     }
 
     return(
-        <>
         <View style={styles.container}>
-         
-            <Text>{itemData}</Text>
-            {/* {console.log("itemData:",itemData)} */}
-            {/* {console.log("picture URL:",itemData[0])} */}
-            {/* <Image style={styles.image1} source={{uri:itemData.itemPic1}}></Image> */}
           
+            {itemData===undefined ? null 
+            :
+            <View style={styles.itemContainer}>
+                <Image style={styles.image1} source={{uri:(itemData[0].itemPic1)}}></Image>
+                <View style={styles.itemInfoContainer}>
+                    <Text style={styles.itemText}>{itemData[0].itemName}</Text>
+                    <Text style={styles.itemText}>Price: ${itemData[0].itemPrice}</Text>
+                    <Text style={styles.itemText}>In Stock Buy Now!</Text>
+                </View>
+            </View>
+            }
+            
         </View>
-        </>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        alignSelf:"center",       
-  
+        // alignSelf:"center",       
+        
     },
     image1:{
         width:"50%",
         height:150,
+    },
+    itemContainer:{
+        flexDirection:"row",
+    },
+    itemInfoContainer:{
+        // flexDirection:"column",
+        margin:5,
+        
+    },
+    itemText:{
+        fontSize:15,
     }
   });
