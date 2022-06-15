019e470b887a48bb0f6527b01544d4b41d9cb57b
Wee Tien Guan <98148142+tgweeSDICohort4@users.noreply.github.com>
2022-06-15 23:26:48 +0800

    add searchbar.js
    -add search bar (no functionality yet)
    -rename containers directory to screens
    -rename displayitem.js to displayItem.js
    -add activity indicator at initial app start (to indicate data loading)
    -add cart icon (no functionality yet)

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,18 @@
-import { StyleSheet, ScrollView, RefreshControl, TextInput } from 'react-native';
+import { StyleSheet, ScrollView, RefreshControl, ActivityIndicator,View, Image } from 'react-native';
 import {useCallback, useEffect, useState} from "react";
 import GetData from "../components/getData";
-import DisplayItem from "../components/displayitem";
+import DisplayItem from "../components/displayItem";
+import SearchBar from "../components/searchBar"
+import CartIcon from "../../assets/cart-icon.png"
+
 
 
 export default function AccountScreen() {
 
     const [itemData, setItemData]=useState();
     const [refreshing, setRefreshing]=useState(false);
-    const [searchText, onChangeText]=useState();
+    const [searchText, setSearchText]=useState("");
+    const [clicked, setClicked]=useState();
 
     useEffect(()=>{
       
@@ -29,19 +33,24 @@ export default function AccountScreen() {
     }
 
     return(
+        <>
+        <View style={styles.container1}>
+            <SearchBar
+                searchText={searchText}
+                setSearchText={setSearchText}
+                clicked={clicked}
+                setClicked={setClicked}
+            />
+            <Image style={styles.cartIconContainer} source={CartIcon}/>
+        </View>
+        
         <ScrollView 
-            style={styles.container}
+            style={styles.container2}
             refreshControl={
                 <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>
             }>
             
-            <TextInput 
-                style={styles.searchBox}
-                onChangeText={onChangeText}
-                value={searchText}
-                />
-            
-            {itemData===undefined ? null 
+            {itemData===undefined ? <ActivityIndicator size="large"/>
             :
             itemData.map((itemData, index)=>(
                 <DisplayItem itemData={itemData} key={index}/>
@@ -49,21 +58,26 @@ export default function AccountScreen() {
             }
             
         </ScrollView>
+        </>
     )
 }
 
 const styles=StyleSheet.create({
-    container:{
+    container1:{
+        backgroundColor:"#FDD100",
+        flexDirection:"row",
+        
+    },
+    container2:{
         backgroundColor:"#FDD100",
     },
-    searchBox:{
-        borderRadius:20,
-        width:250,
-        height:30,
-        backgroundColor:"#ffffc0",
-        alignSelf:"center",
-        marginTop:5,
-        paddingLeft:10,
-        paddingRight:10,
+    cartIconContainer:{
+        width:40,
+        height:40,
+        position:"absolute",
+        right:0,
+        top:3,
+        
     }
+
 })
