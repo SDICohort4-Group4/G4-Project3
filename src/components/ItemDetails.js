dd6aabfe2b48c481123fc9cb9d70437c5f71787d
PKthePK <pkwoo90@gmail.com>
2022-06-16 22:00:54 +0800

    Change imageCon styling from flex to windowHeight

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,60 +1,67 @@
-import { StyleSheet, Text, View, TextInput, Image,ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, ScrollView, Dimensions } from 'react-native';
 import { useContext, useEffect, useState } from "react";
 import { Feather } from "@expo/vector-icons";
 import noImage from "../../assets/photo-soon.jpg";
 import { DataTable } from 'react-native-paper';
 
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').Width;
+
 export default function ItemDetails({route, navigation}) {
     // check what data/format of data is given
     console.log(route.params.itemInfo);
 
+
+    
     return(
-        <View style = {styles.container}>
+        <ScrollView>
+            <View style = {styles.container}>
 
-            <View style = {styles.imageCon}>
-                {route.params.itemInfo.itemPic1? 
-                    <Image style = {styles.image} source = {{uri: route.params.itemInfo.itemPic1}}></Image>
-                :
-                    <Image style = {styles.image} source = {noImage}></Image>
-                }
+                <View style = {styles.imageCon}>
+                    {route.params.itemInfo.itemPic1? 
+                        <Image style = {styles.image} source = {{uri: route.params.itemInfo.itemPic1}}></Image>
+                    :
+                        <Image style = {styles.image} source = {noImage}></Image>
+                    }
+                </View>
+
+            
+                <View style = {styles.infoCon}>
+                    <DataTable>
+                        <DataTable.Row>
+                            <DataTable.Cell style = {styles.nameCon}>Item : </DataTable.Cell>
+                            <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.itemName} </DataTable.Cell>
+                        </DataTable.Row>
+                        <DataTable.Row>
+                            <DataTable.Cell style = {styles.nameCon}>Description : </DataTable.Cell>
+                            <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.itemDescription} </DataTable.Cell>
+                        </DataTable.Row>
+                        <DataTable.Row>
+                            <DataTable.Cell style = {styles.nameCon}>SKU :</DataTable.Cell>
+                            <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.SKU}</DataTable.Cell>
+                        </DataTable.Row>
+                        <DataTable.Row>
+                            <DataTable.Cell style = {styles.nameCon}>Brand :</DataTable.Cell>
+                            <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.brand}</DataTable.Cell>
+                        </DataTable.Row>
+                        <DataTable.Row>
+                            <DataTable.Cell style = {styles.nameCon}>Stock : </DataTable.Cell>
+                            <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.Qty} </DataTable.Cell>
+                        </DataTable.Row>
+                        <DataTable.Row>
+                            <DataTable.Cell style = {styles.nameCon}>Category : </DataTable.Cell>
+                            <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.itemCategory1}, {route.params.itemInfo.itemCategory2}</DataTable.Cell>
+                        </DataTable.Row>
+                    </DataTable>
+                </View> 
+
+                <View style = {styles.buttonsCon}>
+
+                </View>
+
+
             </View>
-
-           
-            <View style = {styles.infoCon}>
-                <DataTable>
-                    <DataTable.Row>
-                        <DataTable.Cell style = {styles.nameCon}>Item : </DataTable.Cell>
-                        <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.itemName} </DataTable.Cell>
-                    </DataTable.Row>
-                    <DataTable.Row>
-                        <DataTable.Cell style = {styles.nameCon}>Description : </DataTable.Cell>
-                        <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.itemDescription} </DataTable.Cell>
-                    </DataTable.Row>
-                    <DataTable.Row>
-                        <DataTable.Cell style = {styles.nameCon}>SKU :</DataTable.Cell>
-                        <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.SKU}</DataTable.Cell>
-                    </DataTable.Row>
-                    <DataTable.Row>
-                        <DataTable.Cell style = {styles.nameCon}>Brand :</DataTable.Cell>
-                        <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.brand}</DataTable.Cell>
-                    </DataTable.Row>
-                    <DataTable.Row>
-                        <DataTable.Cell style = {styles.nameCon}>Stock : </DataTable.Cell>
-                        <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.Qty} </DataTable.Cell>
-                    </DataTable.Row>
-                    <DataTable.Row>
-                        <DataTable.Cell style = {styles.nameCon}>Category : </DataTable.Cell>
-                        <DataTable.Cell style = {styles.valueCon}>{route.params.itemInfo.itemCategory1}, {route.params.itemInfo.itemCategory2}</DataTable.Cell>
-                    </DataTable.Row>
-                </DataTable>
-            </View> 
-
-            <View style = {styles.buttonsCon}>
-
-            </View>
-
-
-        </View>
+        </ScrollView>
     )
 }
 
@@ -77,7 +84,8 @@ const styles = StyleSheet.create({
     },
     
     imageCon:{
-        flex: 5,
+        // flex: 5,
+        height: windowHeight*0.4,
         width: '100%',
     },
 
@@ -95,7 +103,7 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     valueCon: {
-        flex: 3
+        flex: 3,
     },
 
     buttonsCon:{
