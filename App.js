d7dca2d6b5b98f41b0841d306b8fddfdf7fde090
Wee Tien Guan <98148142+tgweeSDICohort4@users.noreply.github.com>
2022-07-05 23:57:06 +0800

    initial setup for stripe

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
 import { StyleSheet, View, Text } from 'react-native';
 import StartScreen from "./src/navigation/start";
 import {DataProvider} from "./src/contexts/AuthContext";
+
+// block text size increase due to android system settings
 Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
 
+// for stripe
+import { StripeProvider } from '@stripe/stripe-react-native';
+
+// remove warnings (warning apparently due to stripe and react native versions)
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["new NativeEventEmitter"]);
+
 export default function App() {
   return (
-    <DataProvider>
-      <View style={styles.container}>
-        <StartScreen/>
-      </View>
-    </DataProvider>
+    <StripeProvider
+      publishableKey="pk_test_51LIAztC2VmHakyazuShGaaWj7GHzkJC1TuPteGJMg3r4uxUdXaa1TIu2vcw7q4kIYPCeZWuITUJxF0ngR65mAjS300GyCVSIAt">
+      <DataProvider>
+        <View style={styles.container}>
+          <StartScreen/>
+        </View>
+      </DataProvider>
+    </StripeProvider>
   );
 }
+
+
+// <DataProvider>
+//  <View style={styles.container}>
+//    <StartScreen/>
+//  </View>
+// </DataProvider> 
+
+
 
 const styles = StyleSheet.create({
   container: {
