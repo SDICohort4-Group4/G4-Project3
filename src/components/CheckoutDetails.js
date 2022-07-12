fc32b76930da6ceb78bfe93ace84f927cc8ccc1b
PKthePK <pkwoo90@gmail.com>
2022-07-13 02:59:19 +0800

    Troubleshooting

--- src/components/CheckoutDetails.js
+++ src/components/CheckoutDetails.js
@@ -8,7 +8,9 @@ import { useFocusEffect, useIsFocused } from '@react-navigation/native';
 
 import AddCalcItemFinalPrice from '../helper/addCalcItemFinalPrice';
 import TotalPayablePrice from '../helper/totalPayablePrice';
-import CountdownTimerLong from '../helper/countdownTimerLong';
+import countdownTimerLong from '../helper/countdownTimerLong';
+
+import CountdownTimer from '../helper/countdownTimer'
 
 const windowHeight = Dimensions.get("window").height;
 const windowWidth = Dimensions.get("window").width;
@@ -23,7 +25,7 @@ export function CheckoutDetails({navigation}){
     const [shortTimer, setShortTimer] = useState(15)
 
     let isFocused = useIsFocused()
-    // const [paySuccessModalVisible, setPaySuccessModalVisible] = useState(false)
+    const [paySuccessModalVisible, setPaySuccessModalVisible] = useState(false)
     
     useEffect(() => {
         if(checkoutArray != undefined){
@@ -50,10 +52,10 @@ export function CheckoutDetails({navigation}){
         },[navigation])
     )
 
-    useEffect(() => {
-        CountdownTimerLong(longTimer, navigation, setLongTimer);
-        // shortCountdownTImer(longTimer, navigation, setLongTimer);
-    },[isFocused])
+    // useEffect(() => {
+    //     countdownTimerLong(longTimer, navigation, setLongTimer);
+    //     // shortCountdownTImer(longTimer, navigation, setLongTimer);
+    // },[isFocused])
 
     // function PaySuccessModal(){
     //     return(
@@ -90,8 +92,12 @@ export function CheckoutDetails({navigation}){
                         <View style = {styles.paymentContainer}>
                             <Text style = {styles.totalPayableLabel}>Total Payable:</Text>
                             <Text style = {styles.totalPayablePrice}>${totalPrice.toFixed(2)}</Text>
+
                         </View>
-                        <Text style = {styles.countdownTimer}>Timeout in {longTimer} seconds</Text>
+                        {/* <Text style = {styles.countdownTimer}>Timeout in {longTimer} seconds</Text> */}
+                        <CountdownTimer
+                            navigation = {navigation}
+                        />
 
                     </View>
                     <ScrollView contentContainerStyle = {{flexGrow: 1}} style = {styles.checkoutContainer}>
@@ -111,7 +117,10 @@ export function CheckoutDetails({navigation}){
                     {/* {returnPage()} */}
                     <Text>No items available for checkout</Text>
                     <Text style = {styles.ShoppingButton} onPress = {() => {navigation.navigate('cartItems')}}>Go back to Cart</Text>
-                    <Text style = {styles.countdownTimer}>Return to Cart in {longTimer} seconds</Text>
+                    {/* <Text style = {styles.countdownTimer}>Return to Cart in {longTimer} seconds</Text> */}
+                    <CountdownTimer
+                        navigation = {navigation}
+                    />
                 </View>
             }
         </View>
